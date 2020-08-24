4f9ed40ccb16a68655bb67f7d7c2e31a1669fd0f
unknown <td.thanh2207@gmail.com>
2020-08-24 22:12:28 +0700

    add redux to Filter component

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,5 +1,6 @@
 import { FETCH_PRODUCTS } from "../types";
-
+import { FILTER_PRODUCTS_BY_COLOR } from "../types"
+import { ORDER_PRODUCTS_BY_PRICE } from './../types';
 export const fetchProducts = () => async(dispatch) => {
     const res = await fetch("/api/products");
     const data = await res.json();
@@ -9,3 +10,34 @@ export const fetchProducts = () => async(dispatch) => {
         payload: data,
     });
 };
+export const filterProducts = (products, color) => (dispatch) => {
+    dispatch({
+        type: FILTER_PRODUCTS_BY_COLOR,
+        payload: {
+            color: color,
+            items: color === "" ?
+                products : products.filter((x) => x.availableColor.indexOf(color) >= 0),
+        },
+    });
+};
+export const sortProducts = (filteredProducts, sort) => (dispatch) => {
+    const sortedProducts = filteredProducts.slice();
+    if (sort === "latest") {
+        sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1))
+    } else {
+        sortedProducts.sort((a, b) => (
+            sort === "lowest" ?
+            a.price > b.price ?
+            1 : -1 :
+            a.price > b.price ?
+            -1 : 1
+        ))
+    }
+    dispatch({
+        type: ORDER_PRODUCTS_BY_PRICE,
+        payload: {
+            sort: sort,
+            items: sortedProducts
+        }
+    })
+}
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,30 +1,48 @@
 import React, { Component } from 'react';
-
-export default class Filter extends Component {
+import { filterProducts } from '../actions/productActions';
+import { sortProducts } from './../actions/productActions';
+import {connect} from 'react-redux'
+class Filter extends Component {
     render() {
         return (
-            <div className="filter">
-                <div className="filter-result">
-                    {this.props.count} Products
+            !this.props.filteredProducts? (
+                <div>Loading...</div>
+                ) : (
+                <div className="filter">
+                    <div className="filter-result">
+                        {this.props.filteredProducts.length} Products
+                    </div>
+                    <div className="filter-sort">
+                        Order {" "}
+                        <select value={this.props.sort} onChange={(e) => this.props.sortProducts(this.props.filteredProducts, e.target.value)}>
+                            <option value="latest">Latest</option>
+                            <option value="lowest">Lowest</option>
+                            <option value="highest">Highest</option>
+                        </select>
+                    </div>
+                    <div className="filter-color">
+                        Color {" "}
+                        <select value={this.props.color} onChange={(e) => this.props.filterProducts(this.props.products, e.target.value)}>
+                            <option value="">All</option>
+                            <option value="Grey">Grey</option>
+                            <option value="Yellow">Yellow</option>
+                            <option value="Red">Red</option>
+                        </select>
+                    </div>
                 </div>
-                <div className="filter-sort">
-                    Order {" "}
-                    <select value={this.props.sort} onChange={this.props.sortProducts}>
-                        <option>Latest</option>
-                        <option value="lowest">Lowest</option>
-                        <option value="highest">Highest</option>
-                    </select>
-                </div>
-                <div className="filter-color">
-                    Color {" "}
-                    <select value={this.props.color} onChange={this.props.filterProducts}>
-                        <option value="">All</option>
-                        <option value="Grey">Grey</option>
-                        <option value="Yellow">Yellow</option>
-                        <option value="Red">Red</option>
-                    </select>
-                </div>
-            </div>
+            )           
         );
     }
 }
+export default  connect(
+    (state) => ({
+        color:state.products.color,
+        sort: state.products.sort,
+        products: state.products.items,
+        filteredProducts: state.products.filteredItems,
+    }),
+    {
+        filterProducts,
+        sortProducts,
+    }
+)(Filter);
